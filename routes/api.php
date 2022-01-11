bf18da7035237f319cfb97cffa1d6b51e51bbc08
David Li <david.li@aspire42.com.au>
2022-01-11 15:25:51 +1100

    API routes set up

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\api\PlantTrackerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/plants', [PlantTrackerController::class, 'index']);
+
+Route::post('/plants', [PlantTrackerController::class, 'store']);
+
+Route::fallback(function () {
+    // fallback here
 });
