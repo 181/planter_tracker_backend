3e71577cb629ee954ad957acd6d6102fc9a74c37
David Li <david.li@aspire42.com.au>
2022-01-11 16:30:44 +1100

    Models set up for Plant and Species

---
+++ app/Models/Plant.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Plant extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['plant_name', 'species_id', 'watering_instruction', 'image'];
+
+    public function species() {
+        return $this->belongsTo('App\Models\Species', 'species_id');
+    }
+}
